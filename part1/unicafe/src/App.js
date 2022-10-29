c07ec4c01dd3632e15e75f486eb1106ac4b8f73d
kasra <k.sorouri@gmail.com>
2022-10-30 00:09:50 +0300

    End of Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let all = good+neutral+bad
 
   return (
    <div>
@@ -12,16 +11,30 @@ const App = () => {
     <button onClick={()=> setGood(good + 1)}>good</button>
     <button onClick={()=> setNeutral(neutral + 1)}>neutral</button>
     <button onClick={()=> setBad(bad + 1)}>Bad</button>
-    <h1>statistics</h1>
-    <p>Good {good}</p>
-    <p>Neutral {neutral}</p>
-    <p>Bad {bad}</p>
-    <p>All {all}</p>
-    <p>Avrage {(good * 1 + neutral * 0 + bad * -1)/all}</p>
-    <p>Positive {good/all * 100} %</p>
+    <Statistics good={good} neutral={neutral} bad={bad} />
    </div>
    
   );
 }
 
 export default App;
+
+const Statistics = (props) => {
+  const {good, neutral, bad} = props;
+  let all = good+neutral+bad;
+
+  return(
+    <div>
+      <h1>statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Avrage {(good * 1 + neutral * 0 + bad * -1)/all}</p>
+      <p>Positive {good/all * 100} %</p>
+
+    </div>
+  )
+
+
+}
