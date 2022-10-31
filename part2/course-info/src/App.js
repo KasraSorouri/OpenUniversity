cb1a980ddd7fa1477ce7fb900652fa66eb51846d
kasra <k.sorouri@gmail.com>
2022-10-31 23:02:56 +0200

    End of Exercise 2.4

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,27 +1,56 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>  <Course course={course} />)}
+    </div>
+  )
 }
 
 export default App
@@ -42,7 +71,7 @@ const Header = (props) => {
   console.log('Heder props -> ', props)
   return (
     <>
-      <h1>{props.course}</h1>
+      <h2>{props.course}</h2>
     </>
   )
 }
@@ -70,7 +99,7 @@ const Total = (props) => {
 
   return (
     <>
-      <p>Number of exercises {total}</p>
+      <h3>Number of exercises {total}</h3>
     </>
   )
 }
