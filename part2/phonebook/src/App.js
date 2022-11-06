d88749262105aeb85e6bf21b9cb5545e6afc5d5e
kasra <k.sorouri@gmail.com>
2022-11-07 01:11:43 +0200

    End of Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Persons from './components/Persons'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
   
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]) 
+    const [persons, setPersons] = useState([]) 
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [search, setSearch] = useState('')
+
+    useEffect(() => {
+      axios
+        .get('http://localHost:3001/persons')
+        .then(respons => {
+          setPersons(respons.data)
+        })
+    },[])
   
     const handelNameChange = (event) => {
       setNewName(event.target.value)
