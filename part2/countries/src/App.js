7962f2fcaf38a84fb7a8102cec32e9082f6cc9a5
kasra <k.sorouri@gmail.com>
2022-11-09 20:50:19 +0200

    End of Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -19,15 +19,14 @@ function App() {
 
   const clickHandler = (choose) => {
     console.log('click -> ',choose)
-    return SetCountry(countries.filter(country => country.cca2.includes(choose)))
+    SetCountry(countries.filter(country => country.cca2.includes(choose)))
   }
-
+  
   return (
     <div>
       <Search search={serach} setSearch={setSearch}/>
       {(results.length !== 1 ) ? <Display  results={results}  clickHandler={clickHandler}  />: <Country country={results} />}
       {(results.length > 1 ) ? <Country country={country} /> : <p></p>}
-
     </div>
   );
 }
@@ -46,8 +45,8 @@ const Search = ({search,setSearch}) => {
   )
 }
 
-const Display = ({results, clickHandler,SetCountry}) => {
- // console.log('Search result length -> ', results.length);
+const Display = ({results, clickHandler}) => {
+//  console.log('Search result length -> ', results.length);
 
   if (results.length > 10) {
     return (<p>Too many matches, specify another filter</p>)
@@ -70,7 +69,7 @@ const Results = ({country, clickHandler}) => {
 }
 
 const Country = (props) => {
-//  console.log('** Country -> ',props.country);
+  console.log('** Country -> ',props);
 
   if (props.country.length === 1) {
     const country = props.country[0]
@@ -83,7 +82,46 @@ const Country = (props) => {
         {Object.values(country.languages).map(lang =><li key={lang}>{lang}</li>)}
         <br />
         <img src={country.flags.png} alt="Flag" />
+        <Weather capital={country.capital} />
     </div>
   );
   }
+}
+
+const Weather = ({capital}) => {
+  const [weather, setWeather] = useState([])
+//  console.log('weather -> ',weather);
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_API_KEY}`
+    console.log('url ->', url);
+    axios
+      .get(url)
+      .then(res => setWeather(res.data))
+    },[capital])
+    let temp;
+    let icon;
+    let description;
+    let realfeel;
+    let windSpeed;
+  console.log(weather);
+    if (weather.main) {
+      temp = (weather.main.temp -273.15).toFixed(1)
+      realfeel = (weather.main.feels_like -273.15).toFixed(1)
+      icon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+      description = weather.weather[0].description
+      windSpeed = weather.wind.speed
+    } else {
+      temp = 0
+      icon = null
+    }
+//    console.log('show wearher icon ->', icon);
+  return(
+    <>
+    <h2>Weather of {weather.name}</h2>
+    <p>temp : {temp} Celcius</p>
+    <p>realfeel : {realfeel} Celcius</p>
+    <img src={icon} alt={description} />
+    <p>wind {windSpeed} m/s</p>
+    </>
+  )
 }
