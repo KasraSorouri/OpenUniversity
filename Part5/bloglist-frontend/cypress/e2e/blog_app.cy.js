f4176b72c216b29115aa14b9828b66ab32275950
kasra <k.sorouri@gmail.com>
2022-12-10 17:31:07 +0200

    End of Exercise 5.18

--- Part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ Part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,4 @@
-describe('First test', () => {
+describe('Blog App Test', () => {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     const user = {
@@ -16,8 +16,36 @@ describe('First test', () => {
       .should('contain', 'username')
       .should('contain', 'password')
       .should('not.contain', 'blogs')
-
   })
 
+  describe('Login test', function () {
+    it('Login fails whith wrong username', function () {
+      cy.get('[name="username"]').type('wrong')
+      cy.get('[name="password"]').type('1234')
+      cy.contains('login').click()
 
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(120, 5, 55)')
+    })
+
+    it('Login fails whith wrong password', function () {
+      cy.get('[name="username"]').type('user1')
+      cy.get('[name="password"]').type('wrong')
+      cy.contains('login').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(120, 5, 55)')
+    })
+
+    it('Successful login', function () {
+      cy.get('[name="username"]').type('user1')
+      cy.get('[name="password"]').type('1234')
+      cy.contains('login').click()
+
+      cy.get('html')
+        .should('contain', 'User Test logged in')
+    })
+  })
 })
