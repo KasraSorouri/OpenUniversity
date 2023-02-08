403343ceb8b95915e8ef47f8309dc06323b0fb79
Kasra Sorouri <k.sorouri@gmail.com>
2023-02-08 21:26:40 +0200

    End of Exercise 8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,17 +98,39 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!,
+    published: Int,
+    author: String!,
+    id: ID!,
+    genres: [String!]
+  }
+
+  type Author {
+    name: String!,
+    id: ID!,
+    born: Int,
+    books: Int
+  }
+
   type Query {
     bookCount: Int!,
-    authorCount: Int!
+    authorCount: Int!,
+    allBooks: [Book!]!
+    allAuthors: [Author!]
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
-  }
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    books: (root) => books.filter(b => b.author === root.name).length
+  }   
 }
 
 const server = new ApolloServer({
