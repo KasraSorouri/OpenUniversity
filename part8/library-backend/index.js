1f1a320638b297ebb85ac223c0d17a460030843e
Kasra Sorouri <k.sorouri@gmail.com>
2023-02-09 10:23:54 +0200

    End of Exercise 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -128,9 +128,9 @@ const typeDefs = `
       published: Int,
       genres: [String]
     ): Book
-    addAuthor(
-      name: String!,
-      born: Int,
+    editAuthor(
+      name: String!
+      born: Int!
     ): Author
   }
 `
@@ -164,6 +164,15 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if(!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.born }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
