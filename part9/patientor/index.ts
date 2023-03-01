8d15465132714ac7a564476b1fcffa9e35cefb63
Kasra Sorouri <k.sorouri@gmail.com>
2023-03-01 15:51:56 +0200

    End of Exercise 9.9

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express();
-app.use(express.json());
+app.use(express.json(),cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong')
 })
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server is running on the port: ${PORT}`);
